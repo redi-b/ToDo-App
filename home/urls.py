340fbc8272d8f04c441623f4f7b2dc92e5001c3a
Rediet <66169993+redi-b@users.noreply.github.com>
2020-10-07 17:27:17 +0300

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("/", views.index, name="index"),
     path("add_todo/", views.add_todo, name="add_todo"),
     path('delete/delete_all/', views.delete_all, name="delete_all"),
     path('delete/delete_item/<int:todo_id>', views.delete_item, name="delete_item"),
